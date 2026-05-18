83c45f5d01fe27e2b465429326522913bdf7a96e
ipx12 <ipx12@ukr.net>
2026-05-18 17:03:51 +0300

    feat: initialize App component with basic layout and assets

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,8 @@ import { Camera, FolderClosed } from 'lucide-react'
 function App() {
   return (
     <>
-      <div className="h-screen w-full bg-gray-500 button-box flex flex-col items-center justify-center gap-2">
-        <div>
-          <img src={snakeImg} className="" width="170" height="179" alt="" />
-        </div>
-        <Button size="lg" className='bg-red-600' variant="primary">Button</Button>
-        <Button size="sm" className='bg-green-600' variant="secondary">Button</Button>
-        <Button size="md" className='bg-blue-600' variant="outline">Button</Button>
-        <Button size="xs" className='bg-yellow-600' variant="ghost">Button</Button>
-        <Button size="lg" className='bg-purple-600' variant="destructive"><FolderClosed /> Button</Button>
-        <Button size="lg" className='bg-pink-600' variant="link"><Camera /> Button</Button>
-      </div>
+      <main className="h-screen">
+      </main>
     </>
   )
 }
