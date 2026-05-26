4dce8333b4f4c26b71b4a5737db8a8af3d15c15a
ipx12 <ipx12@ukr.net>
2026-05-26 17:47:28 +0300

    added dialog close button

--- src/components/web/dialogModal/dialogModal.jsx
+++ src/components/web/dialogModal/dialogModal.jsx
@@ -1,18 +1,24 @@
 import { Dialog, DialogClose, DialogContent, DialogTrigger } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { XIcon } from "lucide-react"
 
 export function DialogModal({ className, children, openButton, ...props }) {
   return (
-    <Dialog>
+    <Dialog className={cn('px-8 py-4 ', className)} {...props}>
         <DialogTrigger asChild>
           {openButton}
         </DialogTrigger>
-        <DialogContent className="sm:max-w-sm">
-            {/* <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose> */}
+        <DialogContent className="sm:max-w-sm" showCloseButton={false}>
             {children}
+            <DialogClose asChild>
+              <Button variant="ghost" className="absolute top-3 right-3 hover:bg-transparent hover:scale-110 active:scale-95 transition-all duration-200 cursor-pointer" size="icon-sm">
+                <XIcon className="size-5 text-[#a855f7] transition-colors" strokeWidth={4.5} strokeLinecap="square" />
+                <span className="sr-only">Close</span>
+              </Button>
+            </DialogClose>
         </DialogContent>
     </Dialog>
   )
 }
+
