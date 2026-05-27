b5a5b69694cf3614e760abacd878aab2a6d44d49
ipx12 <ipx12@ukr.net>
2026-05-27 14:43:35 +0300

    fixed conflict file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import snakeImg from './assets/images/firstSnake.png'
+import snakeImg from '@images/firstSnake.png'
 import './App.css'
-import { Button } from './components/ui/button'
+import { Button } from '@components/ui/button'
 import { Camera, FolderClosed } from 'lucide-react'
-import { LastSection } from './components/sections/lastSection/LastSection'
 function App() {
   return (
     <>
-      <main className="h-screen">
-        <LastSection />
-      </main>
+      <main className="h-screen"></main>
     </>
   )
 }
