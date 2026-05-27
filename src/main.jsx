8e8b0fea0d05fd7361b0e2b7e17b42e4a1fdbf23
DenysShramkov <70487050+DenysShramkov@users.noreply.github.com>
2026-05-27 15:10:47 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import './index.css'
 import './i18n/index.js'
 import App from './App.jsx'
 
-createRoot(
-  document.getElementById('root'),
-).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
