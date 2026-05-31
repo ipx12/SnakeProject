136d01f0da1ef3265adb1e5d1f66dbdf022f304f
ipx12 <ipx12@ukr.net>
2026-05-31 10:58:45 +0300

    clear app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import {
-  useEffect,
-  useState,
-} from 'react'
-import { submitContactForm } from './api'
-import i18n from './i18n'
-import { LastSection } from '@/components/sections/lastSection/LastSection'
-
 function App() {
   return (
-    <main className="h-screen">
-      <LastSection />
-    </main>
+    <main className="h-screen"></main>
   )
 }
 
